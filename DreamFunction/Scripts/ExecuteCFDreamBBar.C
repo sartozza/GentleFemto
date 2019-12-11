721cdf32e2f6d572510207f59bc43367b1466eca
Valentina Mantovani Sarti <valentina.mantovani-sarti@tum.de>
2019-12-11 16:22:30 +0100

    Update plot options systematics BBar, add functions for specific BBar QA, update BBar CF with AntiXi

--- DreamFunction/Scripts/ExecuteCFDreamBBar.C
+++ DreamFunction/Scripts/ExecuteCFDreamBBar.C
@@ -1,13 +1,15 @@
 #include "GetCorrelationsNano.C"
 #include "GetCorrelationsNanoBBar.C"
 #include "GetCorrelationsBbarB.C"
+#include "GetCorrelationsNanoBB.C"
 #include "METoSEReweighting.C"
 #include "TSystem.h"
 int main(int argc, char* argv[]) {
   const char* filename = argv[1];
   const char* prefix = argv[2];
   const char* addon = (argv[3]) ? argv[3] : "";
-  GetCorrelationsNanoBBar(filename, prefix, addon);
+   GetCorrelationsNanoBBar(filename, prefix, addon, 0.18, 0.28);
+  //  GetCorrelationsNanoBB(filename, prefix, addon);
 
   return 1;
 }
